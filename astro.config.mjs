182e423db43c13d7ca7bf2beb435b2b2e1661b47
Camilo <camilojpg30@gmail.com>
2024-04-12 18:35:34 -0300

    Cchanges

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://kmicac.github.io',
-  base: './'
+  site: 'https://kmicac.github.io/portfolio-CP',
 });
