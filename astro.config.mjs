0f47b5793f38e068d7648cd1bccdf126b756c542
Camilo <camilojpg30@gmail.com>
2024-04-12 23:32:10 -0300

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://kmicac.github.io/portfolio-CP',
+  output: "server",
+  adapter: vercel()
 });
